3fef807cf386c3969150ebc8035b8d489dca30a5
Matheus Bodo <matheusbodo167@gmail.com>
2026-01-13 15:19:57 -0300

    integracao com api

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaUser, FaLock } from 'react-icons/fa';
+import API from '../../services/api';
 import './login.css';
 
 export function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [users, setUsers] = useState([]);
+    const inputEmail = useRef();
+    const inputPassword = useRef();
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
+    };
+
+    async function getUsers() {
+        const usersApi = await API.get('/usuarios')
+
+        setUsers(usersApi.data);
+    };
+
+    async function createUsers() {
+        console.log('inputEmail');
     }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-950">
@@ -22,6 +40,7 @@ export function LoginPage() {
                             type="email"
                             placeholder="E-mail"
                             onChange={(e) => setUsername(e.target.value)}
+                            ref={inputEmail}
                         />
                         <FaUser className="icon" />
                     </div>
@@ -30,6 +49,7 @@ export function LoginPage() {
                             type="password"
                             placeholder="Senha"
                             onChange={(e) => setPassword(e.target.value)}
+                            ref={inputPassword}
                         />
                         <FaLock className="icon" />
                     </div>
@@ -40,7 +60,11 @@ export function LoginPage() {
                         </label>
                         <a href="#">Esqueci minha senha</a>
                     </div>
-                    <button> Entrar </button>
+                    <button
+                        onClick={createUsers}
+                    >
+                        Entrar
+                    </button>
                     <div className="signup-link">
                         Não tem uma conta? <a href="#">Registre-se</a>
                     </div>
